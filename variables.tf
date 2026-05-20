eb5f3403202d84fe620f86b0c545438bd75d40bc
Hossain <hossain.j.kh.23@gmail.com>
2026-05-20 15:11:58 +0300

    Initial commit: basic Terraform work directory created

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "environment" {
+    description = "Environment name for tagging"
+    type = string
+}
+
+variable "project_id" {
+  type        = string
+  description = "GCP project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "Default GCP region"
+  default     = "us-central1"
+}
+
+variable "terraform_service_account" {
+  type        = string
+  description = "Service account used by Terraform through impersonation"
+}
