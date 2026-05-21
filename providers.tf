5be5171ae2a4cba6ccab07e6908f0b36a63fa41a
Hossain <hossain.j.kh.23@gmail.com>
2026-05-21 07:18:40 +0300

    Added default labels and changed Terraform file structure

--- providers.tf
+++ providers.tf
@@ -1,29 +1,27 @@
-terraform {
-  required_version = "1.12.2"
-
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 7.29.0, < 8.0.0" # Recommended Fabric version range
-    }
-
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = ">= 7.29.0, < 8.0.0" # Recommended Fabric version range
-    }
-  }
-
-  backend "gcs" {} # Further info stored in non-committed env-[ENV_NAME]-gcs.tfbackend file
-}
-
 provider "google" {
   project                     = var.project_id
   region                      = var.region
   impersonate_service_account = var.terraform_service_account
+
+  default_labels = {
+    managed_by  = "terraform" #
+    environment = var.environment
+    repo        = var.tf_repo_name
+    activity    = var.activity_name
+    owner       = var.owner
+  }
 }
 
 provider "google-beta" {
   project                     = var.project_id
   region                      = var.region
   impersonate_service_account = var.terraform_service_account
+
+  default_labels = {
+    managed_by  = "terraform"
+    environment = var.environment
+    repo        = var.tf_repo_name
+    activity    = var.activity_name
+    owner       = var.owner
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,4 @@
-variable "environment" {
-  description = "Environment name for tagging"
-  type        = string
-}
+# Variables for GCP project and backend configuration
 
 variable "project_id" {
   type        = string
@@ -18,4 +15,27 @@ variable "terraform_service_account" {
   type        = string
   description = "Service account used by Terraform through impersonation"
   sensitive   = true
+}
+
+
+# Variables for consistent tagging across resources
+
+variable "environment" {
+  description = "Environment name for tagging"
+  type        = string
+}
+
+variable "owner" {
+  description = "Owner name for tagging"
+  type        = string
+}
+
+variable "tf_repo_name" {
+  type        = string
+  description = "Name of the Terraform code repository for tagging purposes"
+}
+
+variable "activity_name" {
+  type        = string
+  description = "Name of the activity for tagging purposes"
 }
